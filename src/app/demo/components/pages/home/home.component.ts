bdb2144bbeab48a46b791fd3e4bd42c89de3d2d0
Prosper226 <psedgo@gmail.com>
2024-06-11 04:37:01 +0000

    revu graphe et creation page landing accueil

--- src/app/demo/components/pages/home/home.component.ts
+++ src/app/demo/components/pages/home/home.component.ts
@@ -1,42 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {KillersService} from "../../../service/killers.service";
-import {MessageService} from "primeng/api";
-import {NetworkComponent} from "../killers/network/network.component";
-import {CountryService} from "../../../service/country.service";
-import {ConvictionsService} from "../../../service/convictions.service";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
-    providers: [MessageService, NetworkComponent, KillersService, CountryService, ConvictionsService]
 })
 export class HomeComponent implements OnInit {
     killers: any[] = [];
 
-    constructor(private killerService: KillersService, private router: Router) { }
+    constructor() { }
 
     ngOnInit(): void {
         console.log('here is home page.')
-        this.loadKillers();
-    }
-
-    loadKillers(): void {
-        this.killerService.getAllKillersForHome().subscribe(data => {
-            this.killers = data;
-        });
-    }
-
-    viewKiller(id: string): void {
-        this.router.navigate(['/killers', id]);
-    }
-
-    addKiller(): void {
-        this.router.navigate(['/killers/add']);
-    }
-
-    viewGraph(): void {
-        this.router.navigate(['/graph/complete']);
     }
 }
